4e535988cdb36d90408d9d193be170bd009d41aa
Sanjar Kamaliddinov <sanjarkama26@gmail.com>
2024-10-13 20:28:00 +0500

    alhamdulillah

--- src/App.jsx
+++ src/App.jsx
@@ -1,123 +1,182 @@
-import './App.css'
-import { useState } from 'react'
-import Popup from "./components/popup/Popup"
-import { Chart as ChartJS } from 'chart.js/auto'
-import { Line } from 'react-chartjs-2'
-import GraphData from "./data/data.json"
-// import SecondGraphData from "./data/fackedata.json"
+// import './App.css'
+// import { useState } from 'react'
+// import Popup from "./components/popup/Popup"
+// import { Chart as ChartJS } from 'chart.js/auto'
+// import { Line } from 'react-chartjs-2'
+// import GraphData from "./data/data.json"
+// // import SecondGraphData from "./data/fackedata.json"
 
-import NavSettingsIcon from "./assets/settings.svg"
-import NavNotificationsIcon from "./assets/notifications.svg"
-import AddIcon from "./assets/AddIcon.svg"
-import CashIcon from "./assets/CashIcon.svg"
-import PremiumIcon from "./assets/PremiumSpeedIcon.svg"
+// import NavSettingsIcon from "./assets/settings.svg"
+// import NavNotificationsIcon from "./assets/notifications.svg"
+// import AddIcon from "./assets/AddIcon.svg"
+// import CashIcon from "./assets/CashIcon.svg"
+// import PremiumIcon from "./assets/PremiumSpeedIcon.svg"
 
-import HomeIcon from "./assets/footericons/home.svg"
-import MoreIcon from "./assets/footericons/more.svg"
-import AnaliticsIcon from "./assets/footericons/analitics.svg"
-import PlaningIcon from "./assets/footericons/planing.svg"
+// import HomeIcon from "./assets/footericons/home.svg"
+// import MoreIcon from "./assets/footericons/more.svg"
+// import AnaliticsIcon from "./assets/footericons/analitics.svg"
+// import PlaningIcon from "./assets/footericons/planing.svg"
 
 
-function App() {
+// function App() {
+//   const [transactions, setTransactions] = useState([])
+//   const [balance, setBalance] = useState(0)
+//   const [isPopUpVisible, setPopUpVisible] = useState(false)
+
+//   const openPopUp = () => {
+//     setPopUpVisible(true)
+//   }
+
+//   const closePopUp = () => {
+//     setPopUpVisible(false)
+//   }
+
+
+
+
+//   return (
+//     <>
+//       <Popup isPopUpVisible={isPopUpVisible} closePopUp={closePopUp} />
+
+//       <div className="dashboard page">
+//         <nav>
+//           <div className="container">
+//             <p>Expenses</p>
+//             <div className='nav-icons'>
+//               <img src={NavNotificationsIcon} alt="" />
+//               <img src={NavSettingsIcon} alt="" />
+//             </div>
+//           </div>
+//         </nav>
+//         <main>
+//           <div className="container">
+//             <section className='accounts-menu'>
+//               <div className="cashaccount card" >
+//                 <img src={CashIcon} alt="" />
+//                 <p className='account-title'>CASH</p>
+//                 <p className='account-balance'>{balance}</p>
+//                 <p>USD</p>
+//               </div>
+//               <div className="addaccount card" >
+
+//                 <img src={AddIcon} alt="" />
+//                 <p>Add Account</p>
+//               </div>
+//             </section>
+//             <section className='premium'>
+//               <div className="flex">
+//                 <img src={PremiumIcon} alt="" />
+//                 <div className="premium-text">
+//                   <h2>Wallet for your business</h2>
+//                   <p>Wallet-like app for entrepreneurs and small business.</p>
+//                 </div>
+//               </div>
+//               <button className='getpremium-btn'>Get Premium</button>
+//             </section>
+//             <section className='trend'>
+//               <h3>Balance Trend</h3>
+
+//               <Line data={
+//                 {
+//                   labels: GraphData.map((data) => data.label),
+//                   datasets: [{
+//                     label: "Money",
+//                     data: GraphData.map((data) => data.value),
+//                     backgroundColor: "#0a84fc",
+//                     borderColor: "#0a84fc",
+//                   }, ]
+//                 }}></Line>
+
+//             </section>
+//           </div>
+//         </main>
+//         <footer className='menu'>
+//           <div className="container">
+//             <div className='menu-wrapper'>
+//               <div className='menu__item'>
+//                 <img src={HomeIcon} alt="" />
+//                 <p>Home</p>
+//               </div>
+//               <div className='menu__item'>
+//                 <img src={PlaningIcon} alt="" />
+//                 <p>Planing</p>
+//               </div>
+//               <button onClick={openPopUp} className='action-popup'> <img src={AddIcon} alt="" /> </button>
+//               <div className='menu__item'>
+//                 <img src={AnaliticsIcon} alt="" />
+//                 <p>Statistics</p>
+//               </div>
+//               <div className='menu__item'>
+//                 <img src={MoreIcon} alt="" />
+//                 <p>More</p>
+//               </div>
+//             </div>
+//           </div>
+//         </footer>
+//       </div>
+//     </>
+
+
+//   )
+// }
+
+// export default App
+
+
+import { useState } from "react"
+import { useRef } from "react"
+
+const App = () => {
+
   const [transactions, setTransactions] = useState([])
-  const [balance, setBalance] = useState(0)
-  const [isPopUpVisible, setPopUpVisible] = useState(false)
+  const [Change, setChange] = useState(0)
+  const inpRef = useRef(null)
 
-  const openPopUp = () => {
-    setPopUpVisible(true)
+  function Income() {
+    let amount = Number(inpRef.current.value);
+    if (amount) {
+      setChange(Change + amount);
+      setTransactions([transactions, {type:"Income", amount}]);
+      inpRef.current.value = "";
+    }
   }
 
-  const closePopUp = () => {
-    setPopUpVisible(false)
+  function Expense() {
+    let amount = Number(inpRef.current.value);
+    if (amount) {
+      setChange(Change - amount);
+      setTransactions([transactions, {type:"Expense", amount}]);
+      inpRef.current.value = "";
+    }
   }
-
-
-
 
   return (
     <>
-      <Popup isPopUpVisible={isPopUpVisible} closePopUp={closePopUp} />
+      <p>Over All: {Change}</p>
+      <input type="text" id="inp" ref={inpRef} />
+      <button onClick={Income}>Inc</button>
+      <button onClick={Expense}>Exp</button>
 
-      <div className="dashboard page">
-        <nav>
-          <div className="container">
-            <p>Expenses</p>
-            <div className='nav-icons'>
-              <img src={NavNotificationsIcon} alt="" />
-              <img src={NavSettingsIcon} alt="" />
-            </div>
-          </div>
-        </nav>
-        <main>
-          <div className="container">
-            <section className='accounts-menu'>
-              <div className="cashaccount card" >
-                <img src={CashIcon} alt="" />
-                <p className='account-title'>CASH</p>
-                <p className='account-balance'>{balance}</p>
-                <p>USD</p>
-              </div>
-              <div className="addaccount card" >
-
-                <img src={AddIcon} alt="" />
-                <p>Add Account</p>
-              </div>
-            </section>
-            <section className='premium'>
-              <div className="flex">
-                <img src={PremiumIcon} alt="" />
-                <div className="premium-text">
-                  <h2>Wallet for your business</h2>
-                  <p>Wallet-like app for entrepreneurs and small business.</p>
-                </div>
-              </div>
-              <button className='getpremium-btn'>Get Premium</button>
-            </section>
-            <section className='trend'>
-              <h3>Balance Trend</h3>
-
-              <Line data={
-                {
-                  labels: GraphData.map((data) => data.label),
-                  datasets: [{
-                    label: "Money",
-                    data: GraphData.map((data) => data.value),
-                    backgroundColor: "#0a84fc",
-                    borderColor: "#0a84fc",
-                  }, ]
-                }}></Line>
-
-            </section>
-          </div>
-        </main>
-        <footer className='menu'>
-          <div className="container">
-            <div className='menu-wrapper'>
-              <div className='menu__item'>
-                <img src={HomeIcon} alt="" />
-                <p>Home</p>
-              </div>
-              <div className='menu__item'>
-                <img src={PlaningIcon} alt="" />
-                <p>Planing</p>
-              </div>
-              <button onClick={openPopUp} className='action-popup'> <img src={AddIcon} alt="" /> </button>
-              <div className='menu__item'>
-                <img src={AnaliticsIcon} alt="" />
-                <p>Statistics</p>
-              </div>
-              <div className='menu__item'>
-                <img src={MoreIcon} alt="" />
-                <p>More</p>
-              </div>
-            </div>
-          </div>
-        </footer>
+      <div>
+        <ul>
+          {
+            transactions.map((transaction, i) => (
+              <li key={i}>{transaction.type}:{transaction.amount}</li>
+            ))
+          }
+        </ul>
       </div>
+
     </>
-
-
   )
 }
 
 export default App
+
+
+
+// const [Count, setCount] = useState(0)
+
+// function Counter() {
+//   setCount(Count + 1)
+// }
--- src/components/popup/Popup.jsx
+++ src/components/popup/Popup.jsx
@@ -1,29 +1,16 @@
-// import React from 'react'
-import "./Popup.css"
+// // import React from 'react'
+// import "./Popup.css"
 
-const Popup = ({ isPopUpVisible, closePopUp }) => {
-    return (
-        <div className={`popup ${isPopUpVisible ? "active" : ""}`}>
-            <div className='popup-nav'>
-                <button className='cancel-btn'>Cancel</button>
-                <strong>Add Record</strong>
-                <button onClick={closePopUp} className='save-btn'>Save</button>
-            </div>
-            {/* <div className="category">
-                <select name="type" id="">
-                    <option value="Expense" id="">Expense</option>
-                    <option value="Income" id="">Income</option>
-                    <option value="Transfer" id="">Transfer</option>
-                </select>
-            </div>
-            <div className="details">
-                <select name="type" id="">
-                    <option value="">Cash</option>
-                    <option value="" id="">Bank Account</option>
-                </select>
-            </div> */}
-        </div>
-    )
-}
+// const Popup = ({ isPopUpVisible, closePopUp }) => {
+//     return (
+//         <div className={`popup ${isPopUpVisible ? "active" : ""}`}>
+//             <div className='popup-nav'>
+//                 <button className='cancel-btn'>Cancel</button>
+//                 <strong>Add Record</strong>
+//                 <button onClick={closePopUp} className='save-btn'>Save</button>
+//             </div>
+//         </div>
+//     )
+// }
 
-export default Popup
+// export default Popup
