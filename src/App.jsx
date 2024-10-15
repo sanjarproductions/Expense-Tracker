477719fcfd3513456e2dd42993abebd2c973dbf4
Sanjar Kamaliddinov <sanjarkama26@gmail.com>
2024-10-15 17:08:44 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,31 @@ const App = () => {
   const [Currency, setCurrency] = useState("USD")
   const [transactions, setTransactions] = useState([])
   const [Change, setChange] = useState(0)
-  const inpRef = useRef(null)
+  const TransactionAmount = useRef(null)
+  const TransactionLabel = useRef(null)
+
   const date = new Date().toLocaleDateString()
 
   function Income() {
-    let amount = Number(inpRef.current.value);
+    let label = String(TransactionLabel.current.value)
+    let amount = Number(TransactionAmount.current.value);
     if (amount > 0) {
       setChange(Change + amount);
       transactions.unshift({
-        type: "Income",
+        label,
         amount,
         date,
       })
+    }
+    else {
       alert("Should be bigger than zero")
     }
-    inpRef.current.value = "";
+    TransactionLabel.current.value = "";
+    TransactionAmount.current.value = "";
   }
 
   function Expense() {
-    let amount = Number(inpRef.current.value);
+    let amount = Number(TransactionAmount.current.value);
     if (amount > 0) {
       setChange(Change - amount);
       transactions.unshift({
@@ -36,9 +42,9 @@ const App = () => {
         amount,
         date,
       })
-      alert("Should be bigger than zero")
     }
-    inpRef.current.value = "";
+    TransactionLabel.current.value = "";
+    TransactionAmount.current.value = "";
   }
 
   console.log(transactions)
@@ -50,8 +56,8 @@ const App = () => {
           <div className="flex">
             <img className="logo" src="/favicon.svg" alt="" />
             <div>
-              <a href="">Statistics/History</a>
-              <a href="">Premium</a>
+              <a href="#">Statistics/History</a>
+              <a href="#">Premium</a>
             </div>
           </div>
         </div>
@@ -59,6 +65,13 @@ const App = () => {
 
       <main>
         <div className="container">
+
+          {/* <select name="" id="">
+            <option disabled selected>Select</option>
+            <option value="expense"  >Expense</option>
+            <option value="income" >Income</option>
+          </select> */}
+
           <section className="balance">
             <div className="flex client-balance__flex">
               <img src={cashIcon} className="" alt="" />
@@ -70,8 +83,10 @@ const App = () => {
           </section>
 
           <section className="something">
-            <input placeholder="Label" required type="text" id="inp" />
-            <input placeholder="Amount" required type="text" id="inp" ref={inpRef} />
+            <div>
+              <input placeholder="Label" required type="text" id="inp" ref={TransactionLabel} />
+              <input placeholder="Amount" required type="text" id="inp" ref={TransactionAmount} />
+            </div>
 
             <div className="btns">
               <button onClick={Income}>Income</button>
@@ -84,7 +99,7 @@ const App = () => {
             <ul>
               {
                 transactions.map((transaction, i) => (
-                  <li key={i}>{transaction.type} : {transaction.amount} {Currency}, {transaction.date}</li>
+                  <li key={i}>{transaction.label} : {transaction.amount} {Currency}, {transaction.date}</li>
                 ))
               }
             </ul>
@@ -105,3 +120,17 @@ export default App
 // function Counter() {
 //   setCount(Count + 1)
 // }
+
+
+
+
+
+// Add a Label instead of "Income/Expense"
+// add simbols +/- to make it visually unuderstandable 
+
+// local storage
+// 1000 => 1.000 for visual 
+// History & Statisitcs(pie-chart) page
+// Mobile
+// Starting point (for a lot of people it isn;t gonna be $0)
+// Stripe Premium
